36e4293db17490daec48ad1d88ec590426fb6897
Carlos Morgado <chbm@primatas.org>
2016-01-25 00:15:19 +0000

    better api

--- lib/securetomb.rb
+++ lib/securetomb.rb
@@ -9,53 +9,52 @@ require './lib/securetomb/cyphering'
 
 module SecureTomb
 
-	def self.init(remoteurl, name, path, cypher_name, cypher_params)
-		@remote = Remote.new(remoteurl)
-		if not @remote then
-			puts "don't have a driver for that remote sorry"
-			return nil
+	class Make
+		def initialize(remoteurl)
+			@remote = Remote.new(remoteurl)
+			if not @remote then
+				puts "don't have a driver for that remote sorry"
+				return nil
+			end
 		end
 
-		randomseed = Cyphering.randombytes
+		def init(name, path, cypher_name, cypher_params)
+			randomseed = Cyphering.randombytes
 
 
-		@cypher = Cyphering.new(randomseed, cypher_name, cypher_params) 
+			@cypher = Cyphering.new(randomseed, cypher_name, cypher_params) 
 
-		@remote.put('meta', StringIO.new(JSON.generate({
-			:version => 0,
-			:name => name,
-			:cyphername => cypher_name,
-			:cypherparams => cypher_params,
-			:randomseed => Base64.encode64(randomseed)
-		})))
+			@remote.put('meta', StringIO.new(JSON.generate({
+				:version => 0,
+				:name => name,
+				:cyphername => cypher_name,
+				:cypherparams => cypher_params,
+				:randomseed => Base64.encode64(randomseed)
+			})))
 
-		@fileset = FileSet.fromScratch(name, path, @remote, @cypher)
-		@fileset.putDB(@remote, @cypher)
+			@fileset = FileSet.fromScratch(name, path, @remote, @cypher)
+			@fileset.putDB(@remote, @cypher)
 
-		puts "Initialized #{name}"
-	end
-
-	def self.sync(remoteurl)
-		@remote = Remote.new(remoteurl)
-		if not @remote then # groan
-			puts "don't have a driver for that remote sorry"
-			return nil
+			puts "Initialized #{name}"
 		end
-	
-		begin	
-			metafile = @remote.get('meta')
-		rescue Errno::ENOENT
-			raise NoTomb
+
+		def syncup()	
+			begin	
+				metafile = @remote.get('meta')
+			rescue Errno::ENOENT
+				raise NoTomb
+			end
+			meta = JSON.load(metafile)
+
+			@cypher = Cyphering.new(Base64.decode64(meta["randomseed"]), meta["cyphername"], meta["cypherparams"])
+
+			@fileset = FileSet.new(@remote, @cypher)
+			@fileset.sync(@remote, @cypher)
 		end
-		meta = JSON.load(metafile)
 
-		@cypher = Cyphering.new(Base64.decode64(meta["randomseed"]), meta["cyphername"], meta["cypherparams"])
+		def syncdown(remoteurl)
 
-		@fileset = FileSet.new(@remote, @cypher)
-
-		filetlist = @fileset.diff
-
-		@fileset.sync(filetlist, @remote, @cypher)
+		end
 	end
 
 end
