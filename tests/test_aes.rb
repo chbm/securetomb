bb238d64a4ba66c3dfe3b4713e06312dc6bf851d
Carlos Morgado <chbm@primatas.org>
2016-02-09 21:16:33 +0000

    i'm all reassured

--- tests/test_aes.rb
+++ tests/test_aes.rb
@@ -53,4 +53,52 @@ class TestAes < Test::Unit::TestCase
 	  part2 = decryptor.process(cyphertext.byteslice(98,1024), stateend)
 		assert(part1+part2 == "\x00\x00")	
 	end
+
+	def test_aes_crypt
+		ENV["TOMBPASS"]= "teste"
+
+		aes = SecureTomb::Cyphering.new("\x00" * 32, 'aes256', {})
+		
+
+		origfile = Tempfile.new('aestestin')
+	  IO::write(origfile.path, "\x00123456789\xff" * 5357 )
+		resultfile = Tempfile.new('aestestout')
+		
+		IO::copy_stream(aes.decrypt(aes.encrypt(origfile)), resultfile)
+
+		assert(FileUtils.compare_file(origfile.path, resultfile.path) == true)
+		origfile.close
+		resultfile.close
+
+
+		origfile = Tempfile.new('aestestin')
+	  IO::write(origfile.path, "\x00" * 53571 )
+		resultfile = Tempfile.new('aestestout')
+		
+		IO::copy_stream(aes.decrypt(aes.encrypt(origfile)), resultfile)
+
+		assert(FileUtils.compare_file(origfile.path, resultfile.path) == true)
+		origfile.close
+		resultfile.close
+
+	end
+
+		
+	def test_aes_stress
+		aes = SecureTomb::Cyphering.new("\x00" * 32, 'aes256', {})
+		randomstream = File.open('/dev/random')
+		(1..100).each do 
+			origfile = Tempfile.new('aestestin')
+			resultfile = Tempfile.new('aestestout')
+			IO::copy_stream(randomstream, origfile, Random::rand(1000000))
+			origfile.rewind
+
+			IO::copy_stream(aes.decrypt(aes.encrypt(origfile)), resultfile)
+
+			assert(FileUtils.compare_file(origfile.path, resultfile.path) == true)
+			origfile.close
+			resultfile.close
+		end
+	end
+
 end
--- tests/test_base.rb
+++ tests/test_base.rb
@@ -3,8 +3,5 @@ require "test/unit"
 
 class TestSecureTomb < Test::Unit::TestCase
 
-  def test_fileset
-    assert(FileSet.new)
-  end
 
 end
